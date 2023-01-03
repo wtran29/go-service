8066a183dd4ee1abe6a8911f1a1119f80396f3ba
William Tran <wtran29@gmail.com>
2023-01-03 12:42:37 -0800

    included seed data, db migrations, setup schema packages

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -9,17 +10,75 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"service/business/data/schema"
+	"service/business/sys/database"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := genToken()
+	err := migrate()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func seed() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "tango1337",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
+}
+
+func migrate() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "tango1337",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+
+	return seed()
+
 }
 
 func genToken() error {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.3
+	github.com/ardanlabs/darwin v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/google/uuid v1.3.0
