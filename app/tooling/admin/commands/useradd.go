57f9e2f3f20345fcf7ebe647f83fbf784421ca32
William Tran <wtran29@gmail.com>
2023-01-16 19:39:20 -0800

    completed creating zipkin pod, refactor other existing pods, added vault pod, refactor admin tooling functions

---
+++ app/tooling/admin/commands/useradd.go
@@ -0,0 +1,54 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"net/mail"
+	"time"
+
+	"service/business/core/user"
+	"service/business/core/user/stores/userdb"
+	"service/business/sys/database"
+
+	"go.uber.org/zap"
+)
+
+// UserAdd adds new users into the database.
+func UserAdd(log *zap.SugaredLogger, cfg database.Config, name, email, password string) error {
+	if name == "" || email == "" || password == "" {
+		fmt.Println("help: useradd <name> <email> <password>")
+		return ErrHelp
+	}
+
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	core := user.NewCore(userdb.NewStore(log, db))
+
+	addr, err := mail.ParseAddress(email)
+	if err != nil {
+		return fmt.Errorf("parsing email: %w", err)
+	}
+
+	nu := user.NewUser{
+		Name:            name,
+		Email:           *addr,
+		Password:        password,
+		PasswordConfirm: password,
+		Roles:           []string{user.RoleAdmin, user.RoleUser},
+	}
+
+	usr, err := core.Create(ctx, nu)
+	if err != nil {
+		return fmt.Errorf("create user: %w", err)
+	}
+
+	fmt.Println("user id:", usr.ID)
+	return nil
+}
